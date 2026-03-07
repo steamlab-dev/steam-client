09ed99fb6ae0a9ae072ee971280059a9e4a11cfe
Francisco Castro <fjcastrocs@gmail.com>
2026-03-06 20:16:26 -0600

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,45 @@
+import GenericError from "./common/generic-error";
+import { TypedEventEmitter } from "./common/typed-event-emitter";
 import { isValidRefreshToken } from "./common/utils";
+import ConnectionError, { type ConnectionSubsystem } from "./connection/error";
 import type { DisconnectMsg } from "./connection/event-manager";
 import SteamClientError, { type SteamClientSubsystem } from "./steam-client/error";
+import type {
+  AuthenticationEvents,
+  loginViaCredentialsReq,
+} from "./steam-client/services/authentication";
 import SteamClientImpl from "./steam-client/steam-client";
+import type { SteamClientEvents } from "./steam-client/types";
 import {
   SteamProtocolEResultError,
   SteamProtocolError,
   type SteamProtocolSubsystem,
 } from "./steam-protocol/error";
-import type { SteamMessage } from "./steam-protocol/message-handler/types";
+import type { MsgHandler, SteamMessage } from "./steam-protocol/message-handler/types";
 import ProtoManager from "./steam-protocol/proto-manager";
 
+// Steam language primitives
 export type { SteamProtos, SteamServices } from "./common/steam-language";
 export {
   EMsg,
   EResult,
+  steamEnums as SteamEnums,
 } from "./common/steam-language";
-export { EAuthSessionGuardType } from "./common/steam-language/protos-definitions/steam/steammessages_auth.steamclient";
-export type { ConnectionOptions } from "./connection/types";
 
-export { ProtoManager, isValidRefreshToken };
-export { SteamProtocolError, SteamProtocolEResultError };
-export type { SteamProtocolSubsystem };
+// Main client API
+export { SteamClientImpl as SteamClient };
+export type { ConnectionOptions } from "./connection/types";
+export type { SteamClientEvents, AuthenticationEvents, loginViaCredentialsReq };
+export type { DisconnectMsg, SteamMessage, MsgHandler };
+
+// Errors
 export { SteamClientError };
 export type { SteamClientSubsystem };
-export { SteamClientImpl as SteamClient };
-export type { DisconnectMsg };
-export type { SteamMessage };
+export { SteamProtocolError, SteamProtocolEResultError };
+export type { SteamProtocolSubsystem };
+export { ConnectionError };
+export type { ConnectionSubsystem };
+export { GenericError };
+
+// Advanced and utility exports
+export { ProtoManager, TypedEventEmitter, isValidRefreshToken };
