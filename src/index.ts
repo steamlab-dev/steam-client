d2afd9d81913ac90d05de32aa95e668690d097f8
Francisco Castro <fjcastrocs@gmail.com>
2026-03-08 14:00:57 -0500

    consumer  exports

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,24 @@ import GenericError from "./common/generic-error";
 import { TypedEventEmitter } from "./common/typed-event-emitter";
 import { isValidRefreshToken } from "./common/utils";
 import ConnectionError, { type ConnectionSubsystem } from "./connection/error";
-import type { DisconnectMsg } from "./connection/event-manager";
 import SteamClientError, { type SteamClientSubsystem } from "./steam-client/error";
 import SteamClientImpl from "./steam-client/steam-client";
-import type {
-  AuthenticationEvents,
-  LoginViaCredentialsReq,
-  SteamAuthTokens,
-  SteamClientEvents,
-} from "./steam-client/types";
 import {
   SteamProtocolEResultError,
   SteamProtocolError,
   type SteamProtocolSubsystem,
 } from "./steam-protocol/error";
 import ProtoManager from "./steam-protocol/proto-manager";
-import type {
-  MsgHandler,
-  SteamMessage,
-  SteamMessageName,
-  SteamMessages,
-} from "./steam-protocol/types";
 
-// Steam language primitives
-export type { SteamProtos, SteamServices } from "./common/steam-language";
-export {
-  EMsg,
-  EResult,
-  steamEnums as SteamEnums,
-} from "./common/steam-language";
+// Barrel surfaces (consumer-facing)
+export * from "./common/steam-language";
+export { steamEnums as SteamEnums } from "./common/steam-language";
+export * from "./connection/types";
+export * from "./steam-client/types";
+export * from "./steam-protocol/types";
 
 // Main client API
 export { SteamClientImpl as SteamClient };
-export type { ConnectionOptions } from "./connection/types";
-export type { SteamClientEvents, AuthenticationEvents, LoginViaCredentialsReq, SteamAuthTokens };
-export type { DisconnectMsg, MsgHandler, SteamMessage, SteamMessageName, SteamMessages };
 
 // Errors
 export { SteamClientError };
